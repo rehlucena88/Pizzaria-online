45b4ac133dd57df77f796c2f79ab902b66547680
Renato Lucena <reh.lucena88@gmail.com>
2022-01-01 13:43:54 -0300

    Realizado o mapeamento do json, clonado a estrutura html e adicionado as infos na estrutura html

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);           
     //quando usamos o clonenode clonamos o elemento html para preencher as info e joga-las na tela
 
+    // Adicionado as infos das pizzas
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img; 
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item. price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
 
     c('.pizza-area').append(pizzaItem);//usamos o append para clonar todos itens e nao substituir um por um.
 });
