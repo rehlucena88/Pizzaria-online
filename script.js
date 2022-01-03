2dd46a05d96a856916edfc909b28a605c7810bc1
Renato Lucena <reh.lucena88@gmail.com>
2022-01-03 20:35:49 -0300

    adicionado o modal e previnido a açao de default da tag a

--- script.js
+++ script.js
@@ -12,7 +12,15 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item. price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-
+   // adicionando o modal e previnindo a açao de default da tag a.
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+            e.preventDefault();
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        },300)
+    });
 
     c('.pizza-area').append(pizzaItem);//usamos o append para clonar todos itens e nao substituir um por um.
 });
