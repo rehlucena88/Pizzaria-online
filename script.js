7340a636c121a4323d959ad44d16c5c7b04ce339
Renato Lucena <reh.lucena88@gmail.com>
2022-01-03 21:48:29 -0300

    selecionando o item correto para preencher as infos no modal

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ pizzaJson.map((item, index)=>{
     //quando usamos o clonenode clonamos o elemento html para preencher as info e joga-las na tela
 
     // Adicionado as infos das pizzas
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img; 
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item. price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -15,11 +16,20 @@ pizzaJson.map((item, index)=>{
    // adicionando o modal e previnindo a açao de default da tag a.
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
             e.preventDefault();
+            
+    //preenchimento das informaçoes do modal
+     let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--pricearea').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
+    //animaçao criada para suavisar a açao do modal 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
         setTimeout(()=>{
             c('.pizzaWindowArea').style.opacity = 1;
-        },300)
+        }, 300);
     });
 
     c('.pizza-area').append(pizzaItem);//usamos o append para clonar todos itens e nao substituir um por um.
